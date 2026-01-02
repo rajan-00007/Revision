b9cb224a6bc2eaa45796846157e1d71407643a2b
rajan-00007 <rajan.verma@healthligence.com>
2026-01-02 19:10:22 +0530

    Fixed

--- app/zustand/explore/test-2/page.tsx
+++ app/zustand/explore/test-2/page.tsx
@@ -10,22 +10,35 @@ import {
 import useProductStore from "@/store/productsStore";
 import { ActionButton, Pagediv } from "../../products/Styled";
 
-const page = () => {
+const Page = () => {
   const products = useProductStore((state) => state.products);
 
   const topRatedProducts = products.filter(
-   (product) => (product.rating ?? 0) > 4
+    (product) => (product.rating ?? 0) > 4
   );
 
-  return (
-    <>
-    <Pagediv>
-      <div className=" w-[100vw] flex flex-col justify-center items-center">
-        <h1 className="text-[50px] text-center m-2">Rating Filter Data </h1>
-        <h1 className="text-[30px] text-center m-2">Total Products: {topRatedProducts.length}</h1>
+  const handleClearStorage = () => {
+    if (typeof window !== "undefined") {
+      useProductStore.persist?.clearStorage?.();
+    }
+  };
 
-        <ActionButton onClick={useProductStore.persist.clearStorage}>Clear Storage</ActionButton>
+  return (
+    <Pagediv>
+      <div className="w-[100vw] flex flex-col justify-center items-center">
+        <h1 className="text-[50px] text-center m-2">
+          Rating Filter Data
+        </h1>
+
+        <h1 className="text-[30px] text-center m-2">
+          Total Products: {topRatedProducts.length}
+        </h1>
+
+        <ActionButton onClick={handleClearStorage}>
+          Clear Storage
+        </ActionButton>
       </div>
+
       <ProductsGrid>
         {topRatedProducts.map((product) => (
           <ProductLink
@@ -34,15 +47,17 @@ const page = () => {
           >
             <ProductCard>
               <ProductTitle>{product.title}</ProductTitle>
-              <ProductImage src={product.thumbnail} alt={product.title} />
+              <ProductImage
+                src={product.thumbnail}
+                alt={product.title}
+              />
               <ProductTitle>{product.rating}</ProductTitle>
             </ProductCard>
           </ProductLink>
         ))}
       </ProductsGrid>
-      </Pagediv>
-    </>
+    </Pagediv>
   );
 };
 
-export default page;
+export default Page;
